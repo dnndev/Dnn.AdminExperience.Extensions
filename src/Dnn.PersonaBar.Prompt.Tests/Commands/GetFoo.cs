ec19b8f766f67902fa475e36a58cc96fd4cead68
Peter Donker <peter@bring2mind.net>
2017-02-09 17:07:30 +0100

    Testing an extension

---
+++ src/Dnn.PersonaBar.Prompt.Tests/Commands/GetFoo.cs
@@ -0,0 +1,29 @@
+﻿using Dnn.PersonaBar.Prompt.Attributes;
+using Dnn.PersonaBar.Prompt.Common;
+using Dnn.PersonaBar.Prompt.Interfaces;
+using System;
+using Dnn.PersonaBar.Prompt.Models;
+
+namespace Mynamespace.Commands
+{
+    [ConsoleCommand("get-foo", "Test command", new string[] { })]
+    public class GetFoo : ConsoleCommandBase, IConsoleCommand
+    {
+        public string ValidationMessage { get; }
+
+        public void Init(string[] args, global::DotNetNuke.Entities.Portals.PortalSettings portalSettings, global::DotNetNuke.Entities.Users.UserInfo userInfo, int activeTabId)
+        {
+            Initialize(args, portalSettings, userInfo, activeTabId);
+        }
+
+        public bool IsValid()
+        {
+            return true;
+        }
+
+        public ConsoleResultModel Run()
+        {
+            return new ConsoleResultModel("Foo has completed") { };
+        }
+    }
+}
